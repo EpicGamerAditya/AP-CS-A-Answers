11775b1bfeaddff62ecb4717a3a59787798d52b9
EpicGamerAditya <66402720+EpicGamerAditya@users.noreply.github.com>
2021-03-24 11:54:15 -0700

    Create AP CS A 2014 Q2.java

---
+++ AP CS A 2014 Q2.java
@@ -0,0 +1,18 @@
+a) 
+  
+for (int i = 1; i < array.length -1; i++){
+	if ((arr[i] > arr[i-1]) && (arr[i] > arr[i+1]){
+		return i; 
+
+}
+return -1; 
+
+}
+      
+b) 
+    
+int peak = this.getpeakIndex(array)
+if (peak > -1)
+	if (this.isIncreasing(array, peak)
+		if (this.isDecreasing(array, peak)
+			return True; 
