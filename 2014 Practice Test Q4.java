d10da70ab2d547c771d4df4ce2a3300bc11d8a3c
EpicGamerAditya <66402720+EpicGamerAditya@users.noreply.github.com>
2021-03-24 11:58:05 -0700

    Create 2014 Practice Test Q4.java

---
+++ 2014 Practice Test Q4.java
@@ -0,0 +1,23 @@
+a) 
+
+for (i = 0; i < scorelist.size(); i++){
+	if (scorelist.get(i).getscore() == score)
+		scorelist.get(i).increment(); 
+		return False 
+	else if (scorelist.get(i).getscore() > score)
+		scorelist.add(new ScoreInfo (score));
+		return True; 
+
+}
+
+b) 
+
+for (i = 0; i < stuscores.length; i++){
+	this.record(stuscores[i].getscore); 
+			
+
+}
+
+
+
+}
